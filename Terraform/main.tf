04f3febc2ad89c79026128e2b01ac6df1973fabc
jcga5673 <jcga567345@gmail.com>
2022-02-01 17:03:16 -0600

    terraform comments updated

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -24,49 +24,50 @@ module "eks" {
   asg_desired_capacity_group2 = var.asg_desired_capacity_group2
 }
 
-# module "ec2" {
-#   source = "./modules/ec2"
+/* module "ec2" {
+  source = "./modules/ec2"
 
-#   vpc_id_ec2                     = module.networking.vpc_id
-#   subnet_id                      = module.networking.public_subnets_ids
+  vpc_id_ec2                     = module.networking.vpc_id
+  subnet_id                      = module.networking.public_subnets_ids
   
-#   number_of_instances            = var.number_of_instances
-#   ec2_name                       = var.ec2_name
-#   ec2_security_group_name        = var.ec2_security_group_name
-#   ec2_security_group_description = var.ec2_security_group_description
-#   ec2_ami                        = var.ec2_ami
-#   ec2_instance_type              = var.ec2_instance_type
-# }
+  number_of_instances            = var.number_of_instances
+  ec2_name                       = var.ec2_name
+  ec2_security_group_name        = var.ec2_security_group_name
+  ec2_security_group_description = var.ec2_security_group_description
+  ec2_ami                        = var.ec2_ami
+  ec2_instance_type              = var.ec2_instance_type
+}
+ */
 
-#module "rds" {
-#  source = "./modules/rds"
 
-#  vpc_id_rds        = module.networking.vpc_id
-#  subnets_rds        = module.networking.private_subnets_ids
-#
-#  allocated_storage   = var.allocated_storage
-#  db_engine           = var.db_engine
-#  db_port             = var.db_port
-#  engine_version      = var.engine_version
-#  instance_type       = var.instance_type
-#  database_name       = var.database_name
-#  db_username         = var.db_username
-#  db_password         = var.db_password
-#  publicly_accessible = var.publicly_accessible
-#}
+/* module "rds" {
+ source = "./modules/rds"
 
-# module "s3" {
-#   source = "./modules/s3"
+ vpc_id_rds        = module.networking.vpc_id
+ subnets_rds        = module.networking.private_subnets_ids
+ allocated_storage   = var.allocated_storage
+ db_engine           = var.db_engine
+ db_port             = var.db_port
+ engine_version      = var.engine_version
+ instance_type       = var.instance_type
+ database_name       = var.database_name
+ db_username         = var.db_username
+ db_password         = var.db_password
+ publicly_accessible = var.publicly_accessible
+} */
 
-#   vpc_id_s3   = module.networking.vpc_id
-#   subnet_s3 = module.networking.private_subnets_ids
+/* module "s3" {
+  source = "./modules/s3"
 
-#   bucket_prefix = var.bucket_prefix
-#   acl           = var.acl
-#   versioning    = var.versioning
-# }
+  vpc_id_s3   = module.networking.vpc_id
+  subnet_s3 = module.networking.private_subnets_ids
 
-module "redshift" {
+  bucket_prefix = var.bucket_prefix
+  acl           = var.acl
+  versioning    = var.versioning
+} */
+
+/* module "redshift" {
   source = "./modules/redshift"
 
   vpc_id_redshift = module.networking.vpc_id
@@ -84,3 +85,4 @@ module "redshift" {
   db_port_redshift = var.db_port_redshift
   publicly_accessible = var.publicly_accessible
 }
+ */
